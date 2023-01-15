497a3831a83d9f78a878401d91cda301dce281fe
Khant Sithu <100980146+khantseithu@users.noreply.github.com>
2023-01-15 10:30:38 +0000

    add files

---
+++ fundamentals/customizing-example/src/components/Promo.js
@@ -0,0 +1,12 @@
+function Promo() {
+  return (
+    <div className="promo-section">
+      <div>
+        <h1>Don't miss this deal.</h1>
+      </div>
+      <div>Subscribe to my newsletter & get 20% discount.</div>
+    </div>
+  );
+}
+
+export default Promo;
